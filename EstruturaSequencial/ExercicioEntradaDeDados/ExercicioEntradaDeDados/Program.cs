ee778e1e81a02439ad1d1e3866d59a499674c674
andrelu00 <andrelu0022@gmail.com>
2020-03-17 17:51:01 -0300

    Add files via upload

---
+++ EstruturaSequencial/ExercicioEntradaDeDados/ExercicioEntradaDeDados/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioEntradaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Entre com seu nome completo:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int qtdquartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre seu ultimo nome, idade e altura(mesma linha):");
+            string[] v = Console.ReadLine().Split(' ');
+            string LName = v[0];
+            int idade = int.Parse(v[1]);
+            double altura = double.Parse(v[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(qtdquartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(LName);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
