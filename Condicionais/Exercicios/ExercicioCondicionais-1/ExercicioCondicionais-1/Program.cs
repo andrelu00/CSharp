601ce2724bc36d9be552ff08bffe65438ce8dfce
andrelu00 <andrelu0022@gmail.com>
2020-03-17 18:01:44 -0300

    Add files via upload

---
+++ Condicionais/Exercicios/ExercicioCondicionais-1/ExercicioCondicionais-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioCondicionais_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
