2657a2ad2777d10c0d2143a7a8d26c1d918bf70d
andrelu00 <andrelu0022@gmail.com>
2020-03-17 19:00:59 -0300

    Add files via upload

---
+++ Condicionais/Exercicios/ExercicioCondicionais-1/ExercicioCondicional-4/ExercicioCondicional-4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExercicioCondicional_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Duração de um Jogo!");
+
+            Console.WriteLine("Digite a hora inicial e a final(Separadas por espaço):");
+            string[] h = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(h[0]);
+            int horaFinal = int.Parse(h[1]);
+
+            if (horaInicial > horaFinal) 
+            {
+                int duracao = 24 - (horaInicial - horaFinal);
+                Console.WriteLine("O jogo durou " + duracao + " horas");
+            }
+
+            if(horaInicial < horaFinal) 
+            {
+                int duracao = horaFinal - horaInicial;
+                Console.WriteLine("O jogo durou " + duracao + " horas");
+            }
+
+        }
+    }
+}
