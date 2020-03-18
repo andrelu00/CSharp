1f9ef7abac25f5b2f72ffcfa6328bb60cd8f531d
andrelu00 <andrelu0022@gmail.com>
2020-03-18 20:18:08 -0300

    Add files via upload

---
+++ SistemasDeRepetição/For/ExercicioSobreRepetição-1/ExercicioSobreRepetição-1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExercicioSobreRepetição_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numeros impares num intervalo \n");
+            Console.WriteLine("Digite um numero, entre 1 e 100, para limitar o conjunto:");
+            int X = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lista de numeros: ");
+
+            for(int i=1; i <= X; i++) 
+            {
+                int check = i % 2;
+                
+                if(check != 0) 
+                {
+                    
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
