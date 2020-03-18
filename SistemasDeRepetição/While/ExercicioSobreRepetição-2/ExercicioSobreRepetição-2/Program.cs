2b26ff609193ec36b89d389a735751f1400ee4b9
andrelu00 <andrelu0022@gmail.com>
2020-03-18 15:53:07 -0300

    Add files via upload

---
+++ SistemasDeRepetição/While/ExercicioSobreRepetição-2/ExercicioSobreRepetição-2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ExercicioSobreRepetição_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quadrante de um ponto num plano cartesiano \n");
+
+            Console.WriteLine("Digite as coordenadas inteiras do ponto(Separados por espaço)");
+            string[] cord = Console.ReadLine().Split(' ');
+            int x = int.Parse(cord[0]);
+            int y = int.Parse(cord[1]);
+
+            while(x != 0 && y != 0) 
+            {
+                if(x > 0 && y > 0) 
+                {
+                    Console.WriteLine("Primeiro");
+                }
+                else if(x < 0 && y > 0) 
+                {
+                    Console.WriteLine("Segundo");
+                }
+                else if(x < 0 && y < 0)
+                {
+                    Console.WriteLine("Terceiro");
+                }
+                else 
+                {
+                    Console.WriteLine("Quarto");
+                }
+
+                cord = Console.ReadLine().Split(' ');
+                x = int.Parse(cord[0]);
+                y = int.Parse(cord[1]);
+            }
+            
+        }
+    }
+}
