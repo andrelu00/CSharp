7132280334b69e38ffd6f63e00f95eeaf59c9106
andrelu00 <andrelu0022@gmail.com>
2020-03-19 19:49:18 -0300

    Add files via upload

---
+++ SistemasDeRepetição/For/ExerciciosSobreReptição-6/ExerciciosSobreReptição-6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExerciciosSobreReptição_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcular os multiplos de um número!\n");
+
+            Console.WriteLine("Insira um numero inteiro");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("\nDivisores desse número: ");
+            
+            for(int i=1; i<=num; i++) 
+            {
+                if(num % i == 0) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
