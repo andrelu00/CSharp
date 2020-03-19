a0f54caa7389f2349f0e7b6fbfcbe22acd2b45af
andrelu00 <andrelu0022@gmail.com>
2020-03-19 20:04:30 -0300

    Add files via upload

---
+++ SistemasDeRepetição/For/ExerciciosSobreRepetição-7/ExerciciosSobreRepetição-7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExerciciosSobreRepetição_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Descobrir Quadrados e Cubos de numeros!\n");
+
+            Console.WriteLine("Digite até que numero você quer os quadrados e cubos:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; n >= i; i++) 
+            {
+                int quad = i * i;
+                int cubo = i * i * i;
+
+                Console.WriteLine("{0} {1} {2}", i, quad, cubo );
+            }
+
+        }
+    }
+}
