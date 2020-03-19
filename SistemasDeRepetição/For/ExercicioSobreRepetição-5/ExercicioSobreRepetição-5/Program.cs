21ef2e18de6a8da4b4691dba9943aaff8d085b0e
andrelu00 <andrelu0022@gmail.com>
2020-03-19 19:05:14 -0300

    Add files via upload

---
+++ SistemasDeRepetição/For/ExercicioSobreRepetição-5/ExercicioSobreRepetição-5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExercicioSobreRepetição_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Descobrir o fatorial de um numero!\n");
+            Console.WriteLine("Digite um numero: ");
+            int N = int.Parse(Console.ReadLine());
+
+
+            int fator = 1;
+
+            for (int i = 1; N >= i; i++)    
+            {
+                fator = fator * i;
+                
+            }
+
+            Console.WriteLine("O factório desse número é: "+ fator);
+
+        }
+    }
+}
