b36bc20f5c06713f3377675fd76e6700270188b0
andrelu00 <andrelu0022@gmail.com>
2020-03-20 16:36:04 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Introdução/ExerciciosBasicos/ExerciciosDeIntrodução-2/ExerciciosDeIntrodução-2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosDeIntrodução_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Salario funcionario1 = new Salario();
+            Salario funcionario2 = new Salario();
+
+            Console.WriteLine("Média de salarios entre dois funcionarios: \n");
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            funcionario1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario1.Pagamento = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            
+            Console.WriteLine("\nDados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            funcionario2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionario2.Pagamento = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float SalarioMedio = (funcionario1.Pagamento + funcionario2.Pagamento) / 2;
+            Console.WriteLine("Salário médio = " + SalarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ProgramaçãoOrientadaAoObjeto/Introdução/ExerciciosBasicos/ExerciciosDeIntrodução-2/ExerciciosDeIntrodução-2/Salario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosDeIntrodução_2
+{
+    class Salario
+    {
+        public string Nome;
+        public float Pagamento;
+    }
+}
