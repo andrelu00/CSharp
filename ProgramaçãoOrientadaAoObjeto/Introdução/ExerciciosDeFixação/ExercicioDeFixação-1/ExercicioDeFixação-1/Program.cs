fd9467dc0b3c1e9e93676691f47a2e212748f1c6
andrelu00 <andrelu0022@gmail.com>
2020-03-20 16:37:08 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Introdução/ExerciciosDeFixação/ExercicioDeFixação-1/ExercicioDeFixação-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixação_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retãngulo");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("\nAREA: "+ ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERIMETRO: " + ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL: " + ret.Diagonal().ToString("f2", CultureInfo.InvariantCulture));
+        }
+    }
+}
