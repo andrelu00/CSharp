04aee47929fcd4103b8f25c26b3d0382fcecb694
andrelu00 <andrelu0022@gmail.com>
2020-03-21 00:14:30 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Introdução/ExerciciosMembrosEstáticos/ExerciciosMembrosEstaticos-1/ExerciciosMembrosEstaticos-1/ConversorDeMoedas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciciosMembrosEstaticos_1
+{
+    class ConversorDeMoedas
+    {
+             
+        public static double IOF = 6;
+
+        public static double DolarParaReal(double qtdDolar, double cotacaoDolar) 
+        {
+            double total = qtdDolar * cotacaoDolar;
+            return total + (total * IOF / 100);
+        }
+    }
+}
---
+++ ProgramaçãoOrientadaAoObjeto/Introdução/ExerciciosMembrosEstáticos/ExerciciosMembrosEstaticos-1/ExerciciosMembrosEstaticos-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosMembrosEstaticos_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+            Console.Write("Qual a cotação do dólar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos dólares você vai comprar? ");
+            double qtdDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double result = ConversorDeMoedas.DolarParaReal(qtdDolar, cotacaoDolar);
+
+            Console.WriteLine("Valo a ser pago em reais = " + result.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
