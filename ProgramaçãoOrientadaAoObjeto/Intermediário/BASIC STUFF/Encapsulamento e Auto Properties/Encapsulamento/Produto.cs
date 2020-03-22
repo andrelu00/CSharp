729511ef633fb8e46aeaefee80f363d9324c63da
andrelu00 <andrelu0022@gmail.com>
2020-03-22 14:36:28 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/Encapsulamento e Auto Properties/Encapsulamento/Produto.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+namespace Encapsulamento
+{
+    class Produto
+    {
+        private string _nome;   //Não é possivel definir uma auto propertie pois _nome possui lógica prórpia de get e set
+        public double Preco { get; private set; }
+        public int Quantidade { get; private set; }
+
+        // public int MyProperty { get; set; } snipet: "prop", TAB 2X
+
+        public Produto() { }
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        
+
+        //Get e Set para modificar váriaveis em private
+        public string Nome 
+        {
+            get { return _nome;}
+            set
+            {
+                if(value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+      
+
+       
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return _nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/Encapsulamento e Auto Properties/Encapsulamento/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste de encapsulamento");
+
+            Produto p = new Produto("Produto Desconhecido", 500, 10);
+
+            p.Nome = Console.ReadLine();
+
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+
+        }
+    }
+}
