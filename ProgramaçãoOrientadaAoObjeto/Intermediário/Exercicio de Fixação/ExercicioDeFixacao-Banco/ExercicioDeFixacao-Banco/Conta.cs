df601b6ac45b19239cb8455ef69ef4805d0cd9fb
andrelu00 <andrelu0022@gmail.com>
2020-03-24 17:27:46 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/Exercicio de Fixação/ExercicioDeFixacao-Banco/ExercicioDeFixacao-Banco/Conta.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+
+namespace ExercicioDeFixacao_Banco
+{
+    class Conta
+    {
+        public string numConta { get; private set; }
+        public string Titular { get; set; }
+        public float DepositoInicial { get; private set; }
+        
+
+        public Conta(string numero, string nome) 
+        {
+            numConta = numero;
+            Titular = nome;
+        }
+        
+        
+        public Conta(string numero, string nome,float DepositoInicial): this(numero, nome) 
+        {
+            Deposito(DepositoInicial);
+        }
+
+        public float Saldo() 
+        {
+            return DepositoInicial;
+        }
+
+        public void Deposito(float QuantiaAdd) 
+        {
+            DepositoInicial += QuantiaAdd;
+        }
+
+        public void Saque(float QuantiaRet) 
+        {
+            DepositoInicial -= QuantiaRet + 5;
+        }
+
+
+        public override string ToString()
+        {
+            return "Conta " + numConta + ", Titular: " + Titular + ", Saldo: R$" + Saldo().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/Exercicio de Fixação/ExercicioDeFixacao-Banco/ExercicioDeFixacao-Banco/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioDeFixacao_Banco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float depositoInicial;
+            Conta NovaConta = new Conta
+            Console.Write("Entre o número da conta: ");
+            string numConta = Console.ReadLine();
+            Console.Write("Entre o titular da conta: ");
+            string NomeConta = Console.ReadLine();
+            Console.Write("Haverá deposito inicial?(s/n) ");
+            string deposito = Console.ReadLine().ToLower();
+            
+            
+            
+            if (deposito == "s")
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                depositoInicial = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                NovaConta = new Conta(numConta, NomeConta, depositoInicial);
+            
+            }
+            else 
+            {
+                NovaConta = new Conta(numConta, NomeConta);
+            }
+
+            
+
+            Console.WriteLine("\nDados Atualizados: \n" + NovaConta + "\n");
+            
+            Console.Write("Digite um valor para depósito: ");
+            float Deposito = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            NovaConta.Deposito(Deposito);
+
+            Console.WriteLine("Dados Atualizados: \n" + NovaConta + "\n");
+
+            Console.Write("Digite um valor para saque: ");
+            float Saque = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            NovaConta.Saque(Saque);
+
+            Console.WriteLine("Dados Atualizados: \n" + NovaConta + "\n");
+        }
+    }
+}
