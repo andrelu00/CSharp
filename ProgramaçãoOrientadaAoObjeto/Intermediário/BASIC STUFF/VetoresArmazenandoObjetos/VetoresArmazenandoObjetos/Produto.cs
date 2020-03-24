234fae60ad568f93469ed4196d389e12eef2acb6
andrelu00 <andrelu0022@gmail.com>
2020-03-24 20:33:17 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/VetoresArmazenandoObjetos/VetoresArmazenandoObjetos/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace VetoresArmazenandoObjetos
+{
+    class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+
+        public Produto(string name, double price) 
+        {
+            Nome = name;
+            Preco = price;
+        }
+    }
+}
---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/VetoresArmazenandoObjetos/VetoresArmazenandoObjetos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetoresArmazenandoObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+
+            for(int i = 0; i < n; i++) 
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                vect[i] = new Produto(nome, preco);
+            }
+
+            double soma = 0;
+            for(int i =0; i<n; i++) 
+            {
+                soma += vect[i].Preco;
+            }
+
+            double media = soma / n;
+
+            Console.WriteLine(media.ToString("2F", CultureInfo.InvariantCulture));
+        }
+    }
+}
