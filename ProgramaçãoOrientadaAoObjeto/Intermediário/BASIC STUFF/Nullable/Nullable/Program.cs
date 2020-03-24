55343f6f28ab211309a2dc1616ea54682ea3eab0
andrelu00 <andrelu0022@gmail.com>
2020-03-24 19:41:56 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/Nullable/Nullable/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<double> x = null;
+            double? y = 10;
+
+            //Adiquirir valor padrão ou declarado
+            Console.WriteLine(x.GetValueOrDefault()); // x = 0
+            Console.WriteLine(y.GetValueOrDefault()); // y = 10
+
+            Console.WriteLine();
+            
+            // Valor Booleano indicando se tem valor
+            Console.WriteLine(x.HasValue);  // false
+            Console.WriteLine(y.HasValue);  // true
+
+            Console.WriteLine();
+
+            // Escrever o valor da váriavel
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);//impossivel de imprimir, valor nulo
+            }
+            else 
+            {
+                Console.WriteLine("X is null");
+            }
+            if (y.HasValue)
+            {
+                Console.WriteLine(y.Value);
+            }
+            else
+            {
+                Console.WriteLine("Y is null");
+            }
+
+            Console.WriteLine();
+            
+            //Operador de coalescência nula
+            double a = x ?? 5;// Caso X seja nulo atribuir valor a direita 
+            double b = y ?? 5;// Caso Y seja nulo atribuir valor a direita
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
