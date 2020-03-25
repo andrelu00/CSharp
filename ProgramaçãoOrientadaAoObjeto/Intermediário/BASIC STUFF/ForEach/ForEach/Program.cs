43b974b3f686e95540f90034a1c7a2e24e5e7f0e
andrelu00 <andrelu0022@gmail.com>
2020-03-25 19:56:10 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/ForEach/ForEach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vetor = new string[] { "Maria", "Alex", "Bob" };
+
+            foreach(string obj in vetor) //Forma de fazer um for mais fácil
+            {
+                Console.WriteLine(obj);
+            }
+        }
+    }
+}
