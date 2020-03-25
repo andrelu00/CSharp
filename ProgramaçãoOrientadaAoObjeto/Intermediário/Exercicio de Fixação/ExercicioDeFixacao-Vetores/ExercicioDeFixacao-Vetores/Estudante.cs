2a08cd79ca71dcc308c3a295e1041b113fb4cd8f
andrelu00 <andrelu0022@gmail.com>
2020-03-24 21:28:48 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/Exercicio de Fixação/ExercicioDeFixacao-Vetores/ExercicioDeFixacao-Vetores/Estudante.cs
@@ -0,0 +1,22 @@
+﻿
+namespace ExercicioDeFixacao_Vetores
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Estudante(string nome, string email, int quarto)
+        {
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+
+        public override string ToString()
+        {
+            return Quarto + ": " + Nome + ", " + Email;
+        }
+    }
+}
---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/Exercicio de Fixação/ExercicioDeFixacao-Vetores/ExercicioDeFixacao-Vetores/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExercicioDeFixacao_Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos quartos vão ser alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int MaxQuartos = 9;
+            Estudante[] vetor = new Estudante[MaxQuartos];
+
+            Console.WriteLine(); 
+
+            for(int i = 1; i <= n; i++) 
+            {
+                Console.WriteLine("Aluguel " + i);
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                vetor[quarto] = new Estudante(name, email, quarto);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Quartos Ocupados: ");
+
+            for (int i = 0; i < MaxQuartos; i ++) 
+
+            {
+                if (vetor[i] != null) 
+                {
+                    Console.WriteLine(vetor[i]);
+                }
+            }
+        }
+    }
+}
