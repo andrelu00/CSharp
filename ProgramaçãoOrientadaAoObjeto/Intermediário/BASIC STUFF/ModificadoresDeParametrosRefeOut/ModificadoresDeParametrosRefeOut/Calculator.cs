8acb59e9019d3c1fc0d39620e3d659e4b47d1c83
andrelu00 <andrelu0022@gmail.com>
2020-03-25 19:50:10 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/ModificadoresDeParametrosRefeOut/ModificadoresDeParametrosRefeOut/Calculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModificadoresDeParametrosRefeOut
+{
+    class Calculator
+    {
+        public static void Triplo(ref int x) // Faz a váriavel fazer referência á original, ref exige que a variável seja iniciada
+        {
+            x = x * 3;
+        }
+
+        public static void Triple(int origin, out int result) // Out indica a váriavel de saída da função
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/ModificadoresDeParametrosRefeOut/ModificadoresDeParametrosRefeOut/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ModificadoresDeParametrosRefeOut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 10;
+            int triple;
+
+            Calculator.Triplo(ref a); //Faz a váriavel fazer referência a original, ref exige que a variável seja iniciada 
+            Console.WriteLine(a);
+
+            Calculator.Triple(b, out triple); // out indica a váriavel de saída da função
+            Console.WriteLine(triple);
+        }
+    }
+}
