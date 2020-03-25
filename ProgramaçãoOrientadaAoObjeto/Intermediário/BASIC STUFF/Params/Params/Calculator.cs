d2614e084a8b76790b02ee2a2ebbc3f922cf7ccd
andrelu00 <andrelu0022@gmail.com>
2020-03-25 19:05:57 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/Params/Params/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Params
+{
+    class Calculator
+    {
+        public static int Sum(params int[] numbers)//Params diz que essa função pode receber uma quantidade váriavel de valores
+        {
+            int soma = 0;
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                soma += numbers[i];
+            }
+            return soma;
+        }
+
+
+    }
+}
---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/Params/Params/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3);//Posso enviar vários parâmetros para a funão graças qo params
+            int s2 = Calculator.Sum(2, 3, 5);
+
+            Console.WriteLine(s1+"\n"+s2);
+        }
+    }
+}
