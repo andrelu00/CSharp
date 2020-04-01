29314af2884ec0b0696badf800dd74636ce27feb
andrelu00 <andrelu0022@gmail.com>
2020-04-01 13:32:37 -0300

    Add files via upload

---
+++ ProgramaçãoOrientadaAoObjeto/Intermediário/BASIC STUFF/Matriz/Matrizes/Matrizes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3];
+            
+            Console.WriteLine(mat.Length); //Indica o total de elementos da Matriz
+
+            Console.WriteLine(mat.Rank); //Indica o numero de linhas
+
+            Console.WriteLine(mat.GetLength(0)); //Indica o tamanho da Primeira Dimensão da Matriz
+
+            Console.WriteLine(mat.GetLength(1)); //Indica o tamanhoa da Segunda Dimensão da Matriz
+        }
+    }
+}
