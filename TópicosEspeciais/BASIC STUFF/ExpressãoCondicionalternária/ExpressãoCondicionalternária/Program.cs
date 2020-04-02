f195d9109819b69967a1108e7fb1fca79f2ea11e
andrelu00 <andrelu0022@gmail.com>
2020-04-02 12:06:40 -0300

    Add files via upload

---
+++ TópicosEspeciais/BASIC STUFF/ExpressãoCondicionalternária/ExpressãoCondicionalternária/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExpressãoCondicionalternária
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sintaxe: (condição) ? valor_se_verdadeiro : valor_se_falso 
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+
+            double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
