e92b258b96a46ce062c49ce4e3a898d46d17d6ff
andrelu00 <andrelu0022@gmail.com>
2020-04-02 11:44:35 -0300

    Add files via upload

---
+++ TópicosEspeciais/BASIC STUFF/PalavraVar/PalavraVar/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PalavraVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10; //Int
+            var y = 20.00; //Double
+            var z = "Nome"; //String
+            
+            
+            //C# pode inferir o tipo de váriavel mais apropriado para o dado recebido
+        }
+    }
+}
